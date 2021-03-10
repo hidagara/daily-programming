cd11eeb33948e1faafa788a909d6c996b687dbc4
Roman Guseynov <hidagara@gmail.com>
2021-03-10 23:27:54 +0300

    add ip

--- golang-sandbox.go
+++ golang-sandbox.go
@@ -5,15 +5,27 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	fmt.Println("Hello my friends")
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("todos/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe("192.168.3.17:8080", nil))
+}
+
+type Todo struct {
+	Name      string
+	Completed bool
+	Due       time.Time
 }
