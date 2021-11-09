d78d0bafeb6aebaa434e794e0950654ebb8272b4
Roman <hidagara@gmail.com>
2021-11-09 15:18:19 +0300

    upd

---
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Roman Guseynov on 08.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
